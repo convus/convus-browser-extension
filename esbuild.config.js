20b7eb2d9a527282ff33537c6a39382cd7802787
sethherr <seth.william.herr@gmail.com>
2023-03-06 10:49:33 -0800

    Continue working on apple

--- esbuild.config.js
+++ esbuild.config.js
@@ -4,7 +4,7 @@ const fs = require('fs')
 const watch = process.argv.includes('--watch')
 
 // Current options: chrome, firefox
-const target = 'firefox'
+const target = 'safari'
 
 // NOTE: index.html and manifest.json are generated via this script
 // THEY DO NOT UPDATE ON SAVE when watching (the JS does)
