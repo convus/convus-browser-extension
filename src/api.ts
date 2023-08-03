c858e96155c6f0cdfad8a7215adace8bf76c3edc
Seth Herr <seth.william.herr@gmail.com>
2023-08-03 10:09:26 -0700

    ts-standard

--- src/api.ts
+++ src/api.ts
@@ -1,26 +1,26 @@
 import log from './log' // eslint-disable-line
 
-const requestProps = (authToken = "", extraProps = {}) => {
+const requestProps = (authToken = '', extraProps = {}) => {
   const headers = {
     'Content-Type': 'application/json',
-    ...(authToken.length > 0) && { 'Authorization': `Bearer ${authToken}` }
+    ...(authToken.length > 0) && { Authorization: `Bearer ${authToken}` }
   }
 
   const defaultProps = {
     method: 'POST', // because default to some method
     async: true,
-    headers: headers,
+    headers,
     contentType: 'json'
   }
   return { ...defaultProps, ...extraProps }
 }
 
 // Returns true/false
-const isAuthTokenValid = (authUrl, authToken) => new Promise((resolve, reject) => {
+const isAuthTokenValid = async (authUrl, authToken) => await new Promise(async (resolve, reject) => {
   const authStatusUrl = `${authUrl}/status`
 
-  return fetch(authStatusUrl, requestProps(authToken, { method: 'GET' }))
-    .then(response => response.json()
+  return await fetch(authStatusUrl, requestProps(authToken, { method: 'GET' }))
+    .then(async response => await response.json()
       .then((json) => {
         resolve(json.message !== 'missing user' && response.status === 200)
       })
@@ -29,7 +29,7 @@ const isAuthTokenValid = (authUrl, authToken) => new Promise((resolve, reject) =
     })
 })
 
-const getAuthToken = (authUrl, loginFormData) => new Promise((resolve, reject) => {
+const getAuthToken = async (authUrl: RequestInfo | URL, loginFormData: any) => await new Promise(async (resolve, reject) => {
   const rProps = {
     method: 'POST',
     async: true,
@@ -37,8 +37,8 @@ const getAuthToken = (authUrl, loginFormData) => new Promise((resolve, reject) =
     contentType: 'json',
     body: loginFormData
   }
-  return fetch(authUrl, rProps)
-    .then(response => response.json()
+  return await fetch(authUrl, rProps)
+    .then(async response => await response.json()
       .then((json) => {
         resolve(responseFormatter(response, json))
       })
@@ -47,7 +47,7 @@ const getAuthToken = (authUrl, loginFormData) => new Promise((resolve, reject) =
     })
 })
 
-const responseFormatter = (authResponse: any, authJson: any) => {
+const responseFormatter = function (authResponse: any, authJson: any) {
   if (authResponse.status !== 200 || typeof (authJson.review_token) === 'undefined' || authJson.review_token === null) {
     return { message: ['error', authJson.message] }
   } else {
@@ -55,11 +55,11 @@ const responseFormatter = (authResponse: any, authJson: any) => {
   }
 }
 
-const submitRating = (ratingUrl, authToken, ratingFormData) => new Promise((resolve, reject) => {
+const submitRating = async (ratingUrl: RequestInfo | URL, authToken: string | undefined, ratingFormData: any) => await new Promise(async (resolve, reject) => {
   const rProps = requestProps(authToken, { body: ratingFormData })
 
-  return fetch(ratingUrl, rProps)
-    .then(response => response.json()
+  return await fetch(ratingUrl, rProps)
+    .then(async response => await response.json()
       .then((json) => {
         if (response.status === 200) {
           resolve({
@@ -77,7 +77,7 @@ const submitRating = (ratingUrl, authToken, ratingFormData) => new Promise((reso
 })
 
 // Just return an error message that includes the error
-const errorResponse = (e) => {
+const errorResponse = (e: any) => {
   return { success: false, message: ['error', `Error: ${e})`] }
 }
 
